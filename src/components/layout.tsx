a4f0f0254ef2881ddf1ebc4618cf224d6908275d
Muhammad Muhajir <muhajirframe@gmail.com>
2019-02-14 20:01:03 +0700

    Update layout.tsx to use useStaticQuery

--- src/components/layout.tsx
+++ src/components/layout.tsx
@@ -1,5 +1,4 @@
-import { graphql, StaticQuery } from 'gatsby';
-import PropTypes from 'prop-types';
+import { graphql, useStaticQuery } from 'gatsby';
 import React from 'react';
 
 import Header from './header';
@@ -15,46 +14,21 @@ const QUERY = graphql`
   }
 `;
 
-interface IProps {
-  data: {
-    site: {
-      siteMetadata: {
-        title: string;
-      };
-    };
-  };
-  children: React.ReactNode;
-}
-
-// style={{
-//   margin: `0 auto`,
-//   maxWidth: 960,
-//   padding: `0px 1.0875rem 1.45rem`,
-//   paddingTop: 0,
-// }}
-
-const BuildInnerRender: React.FC<IProps> = (props) => (
-  <>
-    <Header siteTitle={props.data.site.siteMetadata.title} />
-    <div>
-      <main>{props.children}</main>
-      <footer>
-        © {new Date().getFullYear()}, Built with
-        {` `}
-        <a href='https://www.gatsbyjs.org'>Gatsby</a>
-      </footer>
-    </div>
-  </>
-);
-
-const Layout: React.FunctionComponent = ({ children }) => (
-  <StaticQuery query={QUERY}>
-    {(data) => <BuildInnerRender data={data}>{children}</BuildInnerRender>}
-  </StaticQuery>
-);
-
-Layout.propTypes = {
-  children: PropTypes.node.isRequired,
+const Layout: React.FunctionComponent = ({ children }) => {
+  const data = useStaticQuery(QUERY);
+  return (
+    <>
+      <Header siteTitle={data.site.siteMetadata.title} />
+      <div>
+        <main>{children}</main>
+        <footer>
+          © {new Date().getFullYear()}, Built with
+          {` `}
+          <a href='https://www.gatsbyjs.org'>Gatsby</a>
+        </footer>
+      </div>
+    </>
+  );
 };
 
 export default Layout;
