cb3afd76e0bbd98ea35dbcd0a648b71539cba785
Muhammad Muhajir <muhajirframe@gmail.com>
2019-02-14 19:33:05 +0700

    Fix layout.tsx to fit typescript

--- src/components/layout.tsx
+++ src/components/layout.tsx
@@ -1,46 +1,61 @@
-import React from "react";
-import PropTypes from "prop-types";
-import { StaticQuery, graphql } from "gatsby";
+import { graphql, StaticQuery } from 'gatsby';
+import PropTypes from 'prop-types';
+import React from 'react';
+import { sitetitle } from '../generated/graphql';
 
-import Header from "./header";
-import "./layout.css";
+import Header from './header';
+import './layout.css';
+
+const QUERY = graphql`
+  query SiteTitleQuery {
+    site {
+      siteMetadata {
+        title
+      }
+    }
+  }
+`;
+
+interface IProps {
+  data: {
+    site: {
+      siteMetadata: {
+        title: string;
+      };
+    };
+  };
+  children: React.ReactNode;
+}
+
+// style={{
+//   margin: `0 auto`,
+//   maxWidth: 960,
+//   padding: `0px 1.0875rem 1.45rem`,
+//   paddingTop: 0,
+// }}
+
+const BuildInnerRender: React.FC<IProps> = (props) => (
+  <>
+    <Header siteTitle={props.data.site.siteMetadata.title} />
+    <div>
+      <main>{props.children}</main>
+      <footer>
+        © {new Date().getFullYear()}, Built with
+        {` `}
+        <a href='https://www.gatsbyjs.org'>Gatsby</a>
+      </footer>
+    </div>
+  </>
+);
 
 const Layout: React.FunctionComponent = ({ children }) => (
-  <StaticQuery
-    query={graphql`
-      query SiteTitleQuery {
-        site {
-          siteMetadata {
-            title
-          }
-        }
-      }
-    `}
-    render={data => (
-      <>
-        <Header siteTitle={data.site.siteMetadata.title} />
-        <div
-          style={{
-            margin: `0 auto`,
-            maxWidth: 960,
-            padding: `0px 1.0875rem 1.45rem`,
-            paddingTop: 0
-          }}
-        >
-          <main>{children}</main>
-          <footer>
-            © {new Date().getFullYear()}, Built with
-            {` `}
-            <a href="https://www.gatsbyjs.org">Gatsby</a>
-          </footer>
-        </div>
-      </>
-    )}
-  />
+  <StaticQuery query={QUERY}>
+    {(data) => <BuildInnerRender data={data}>{children}</BuildInnerRender>}
+  </StaticQuery>
 );
 
 Layout.propTypes = {
-  children: PropTypes.node.isRequired
+  children: PropTypes.node.isRequired,
 };
 
 export default Layout;
