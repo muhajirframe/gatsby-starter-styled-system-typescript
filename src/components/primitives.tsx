7a9aac45ac0948166eb927c1fbd43efc3d8175a7
Muhammad Muhajir <muhajirframe@gmail.com>
2019-02-14 21:02:59 +0700

    Update primitives.tsx

--- src/components/primitives.tsx
+++ src/components/primitives.tsx
@@ -1,1 +1,13 @@
-export { Flex, Box } from 'rebass';
+import { Box as BoxRebass, Flex } from 'rebass';
+import styled from 'styled-components';
+import { color } from '../theme';
+interface IBox {
+  color?: color;
+  bg?: color;
+}
+const Box = styled(BoxRebass)<IBox>``;
+
+/**
+ * Export
+ */
+export { Flex, Box };
