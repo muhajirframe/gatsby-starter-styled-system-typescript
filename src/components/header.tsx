3babe8ddf92a5f537644212d4964d2b8ea20b82e
Muhammad Muhajir <muhajirframe@gmail.com>
2019-02-15 08:27:35 +0700

    Update header

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -1,22 +1,19 @@
 import { Link } from 'gatsby';
 import React from 'react';
+import { Box } from './primitives';
 
 interface IProps {
   siteTitle: string;
 }
 
 const Header: React.FunctionComponent<IProps> = (props) => (
-  <header style={{ background: `rebeccapurple`, marginBottom: `1.45rem` }}>
-    <div
-      style={{ margin: `0 auto`, maxWidth: 960, padding: `1.45rem 1.0875rem` }}
-    >
-      <h1 style={{ margin: 0 }}>
-        <Link to='/' style={{ color: `white`, textDecoration: `none` }}>
-          {props.siteTitle}
-        </Link>
-      </h1>
-    </div>
-  </header>
+  <Box bg='primary' color='primary'>
+    <h1 style={{ margin: 0 }}>
+      <Link to='/' style={{ color: `white`, textDecoration: `none` }}>
+        {props.siteTitle}
+      </Link>
+    </h1>
+  </Box>
 );
 
 export default Header;
